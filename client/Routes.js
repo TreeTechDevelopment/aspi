280b8649442e82f33582bf3036e97c8d38c9f47f
TreeTechDevelopment <65210491+TreeTechDevelopment@users.noreply.github.com>
2020-05-12 19:05:06 -0500

    Add files via upload

--- client/Routes.js
+++ client/Routes.js
@@ -16,6 +16,7 @@ function Routes() {
           </Route>
         </Switch>
     </Router>
+    
     )
 }
 
---
+++ client/src/components/home/FormIS.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function FormIS() {
+    return (
+        <form>
+            <input type="input" placeholder="Usuario"/>
+            <input type="input" placeholder="Contraseña"/>
+            <button>Entrar</button>
+        </form>
+        
+    )
+}
+export default FormIS
