b4339ec698aefb8074646bdf44d1d0513e3bfa63
TreeTechDevelopment <65210491+TreeTechDevelopment@users.noreply.github.com>
2020-06-02 19:33:59 -0500

    Add files via upload

--- client/src/components/login/FormIS.js
+++ client/src/components/login/FormIS.js
@@ -1,56 +1,10 @@
-import React, { useState } from 'react'
-import axios from 'axios';
-
-import { url, messageServerError } from '../../../app.json'
+import React from 'react'
 
 function FormIS() {
-
-    const [userName, setUserName] = useState("")
-    const [password, setPassword] = useState("")
-
-    const handleInputUserName = (e) => setUserName(e.target.value)
-
-    const handleInputPassword = (e) => setPassword(e.target.value)
-
-    const login = (e) => {
-        e.preventDefault()
-        fetchLogin().then(({user, token}) => {
-            setToken(token)
-            setUser(user)
-        }).catch(() => { alert(`messageServerError`) })
-    }
-
-    const setToken = token => window.localStorage.setItem('@token', token )
-
-    const setUser = user => window.localStorage.setItem('@user', user )
-
-    const fetchLogin = async () => {
-        const res = await axios({
-            url: `${url}/users`,
-            method: 'POST',
-            data: {
-                userName,
-                password
-            },
-            timeout: 5000
-        })
-        return res.data
-    }
-
     return (
-        <form id="login-form" onSubmit={login}>
-            <input 
-                type="input" 
-                placeholder="Usuario" 
-                value={userName}
-                onChange={handleInputUserName}
-            />
-            <input 
-                type="password" 
-                placeholder="Contraseña"
-                value={password}
-                onChange={handleInputPassword}
-            />
+        <form id="login-form">
+            <input type="input" placeholder="Usuario"/>
+            <input type="password" placeholder="Contraseña"/>
             <button className="btn btn-primary">Entrar</button>
         </form>
         
--- client/src/components/serviceOrder/Form.js
+++ client/src/components/serviceOrder/Form.js
@@ -1,100 +1,48 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-
-import { url, messageServerError } from '../../../app.json'
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Form = () => {
 
-    const [year, setYear] = useState('a')
-    const [makes, setMakes] = useState([])
-    const [models, setModels] = useState([])
-    const [modelsSelect, setModelsSelect] = useState([])
-    const [motors, setMotors] = useState([])
-    const [make, setMake] = useState('')
-    const [model, setModel] = useState('')
-    const [motor, setMotor] = useState('')  
-    const [infoFetched, setInfoFetched] = useState(false)    
-
-    const handleSelect = (e) => { setYear(e.target.value) }
-
-    const handlePicker = (e) => { 
-        console.log(e)
-    }
-
-    const setData = (models, motors, makes) => {
-        console.log(models)
-        setMakes(makes)
-        setMake(makes[0].name)
-
-        let newModels = models.filter( model => model.make.name === makes[0].name )
-        setModels(models)
-        setModelsSelect(newModels)
-        setModel(newModels[0].name)
-
-        setMotors(motors)
-        setMotor(motors[0].name)
-        setInfoFetched(true)
-    }
-
-    useEffect(() => {
-        fetchCars().then(({ models, motors, makes }) => {
-            setData(models, motors, makes)
-        }).catch((e) => {
-            setInfoFetched(true)
-            alert(`${messageServerError}`)
-        })
-    },[])
-
-    const fetchCars = async () => {
-        const res = await axios({
-            method: 'GET',
-            url : `${url}/cars`,
-            timeout: 5000
-        })
-        return res.data
-    }
-
-    const handleMakeSelect = e => {
-        setMake(e.target.value)
-
-        let newModels = models.filter( model => model.make.name === e.target.value ) 
-        setModelsSelect(newModels)
-    }
-
-    const handleModelSelect = e => setModel(e.target.value)
-
-    const handleMotorSelect = e => setMotor(e.target.value)
 
     return (
+   
         <>
-            {infoFetched && (
-                <>
-                     <h1>Orden de servicio</h1>
-                    <form id="form-service-order">
-                        <label>Marca</label>
-                        <select onChange={handleMakeSelect} value={make}>
-                            {makes.map(make => (
-                                <option key={make._id} value={make.name}>{make.name}</option>
-                            ))}
-                        </select>
-                        <label>Modelo</label>
-                        <select value={model} onChange={handleModelSelect}>
-                            {modelsSelect.map(model => (
-                                <option key={model._id} value={model.name}>{model.name}</option>
-                            ))}
-                        </select>
-                        <label>Motor</label>
-                        <select value={motor} onChange={handleMotorSelect}>
-                            {motors.map(motor => (
-                                <option key={motor._id} value={motor.name}>{motor.name}</option>
-                            ))}
-                        </select>
-                    </form>
-                </>
-            )}
+             <h3>Datos del vehiculo</h3>
+             
+             <form id="form-service-order">
+                <label>Año</label>
+                <div className="input-container-form-">
+                    <input 
+                        type="text"
+                        name="ano"
+                        placeholder="Armadora"
+                    />
+                </div>
+                <label>Modelo</label>
+                <input
+                    type="text"
+                    name="modelo"
+                    placeholder="Modelo"
+                />
+                <label>Cilindraje</label>
+                <input 
+                    type="text"
+                    name="cilindraje"
+                    placeholder="cilindraje"
+                />
+                <label>Motor</label>
+                <input 
+                    type="text"
+                    name="Motor"
+                    placeholder="motor"
+                />
+                <button
+                type="submit"
+                className="fun"
+                ></button>
+            </form>
         </>
      );
 }
  
 export default Form;
-
